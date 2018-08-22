baf4e54653983592218f2296767328f04fd20734
Michael Bright <github.com@mjbright.net>
2018-08-22 19:26:07 +0100

    Added PORT as a variable

--- visualize.sh
+++ visualize.sh
@@ -2,23 +2,23 @@
 # See here for info on 'kubectl proxy' command:
 #   https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands#proxy
 
+#PORT=8001
+PORT=8002
+
 #kubectl proxy --www=/Users/mjb/src/git/brendandburns.gcp-live-k8s-visualizer --www-prefix=/my-mountpoint/ --api-prefix=/api/
 
-#SRC_DIR=~/src/git/brendandburns.gcp-live-k8s-visualizer
 SRC_DIR=~/src/git/mjbright.live-k8s-visualizer
 
 if [ "$1" = "." ];then
-    SRC_DIR=$PWD
+    #SRC_DIR=$PWD
+    SRC_DIR=.
 fi
 
 if [ "$1" = "-l" ];then
     SRC_DIR=~/src/git/larrycai.gcp-live-k8s-visualizer
 fi
 
-
-#kubectl proxy --www=$SRC_DIR --www-prefix=/ --api-prefix=/api/ --port 8001
-#kubectl proxy --www=$SRC_DIR --www-prefix=/viz --api-prefix=/api/
 set -x
-kubectl proxy --www=$SRC_DIR --www-prefix=/ --api-prefix=/api/
+kubectl proxy --www=$SRC_DIR --www-prefix=/ --api-prefix=/api/ --port $PORT
 set +x
 
