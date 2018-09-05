e265803ec735558e461d91d01a84e8603942cbad
Michael Bright <github.com@mjbright.net>
2018-09-05 14:32:43 +0200

    Improved visualizer.sh
    back-ported from codeeurope-microservices

--- visualize.sh
+++ visualize.sh
@@ -7,18 +7,39 @@ PORT=8002
 
 #kubectl proxy --www=/Users/mjb/src/git/brendandburns.gcp-live-k8s-visualizer --www-prefix=/my-mountpoint/ --api-prefix=/api/
 
-# SRC_DIR=~/src/git/GIT_mjbright/live-k8s-visualizer
-SRC_DIR=.
+SRC_DIR=$(dirname $0)
 
+die() {
+    echo "$0: die - $*" >&2
+    exit 1
+}
 
-while [ ! -z "$1" ]; do
+OPTS=""
+
+while [ ! -z "$1" ];do
     case $1 in
-        -l) SRC_DIR=~/src/git/larrycai.gcp-live-k8s-visualizer;;
-        *)  SRC_DIR=$1;;
+        -r)   # Access remotely:
+              IP=$(ip a | awk '!/127.0.0.1/ && / inet / { FS="/"; $0=$2; print $1; exit(0); }')
+              if [ ! -z "$2" ]; then
+                  shift; CLIENT_IP=$1
+              else
+                  CLIENT_IP=0.0.0.0
+	      fi
+              #OPTS+="--accept-hosts='^localhost$,^127\.0\.0\.1$,$CLIENT_IP'"
+              OPTS+=" --address=$IP --accept-hosts='^${CLIENT_IP}$'"
+	      # --accept-hosts='^localhost$,^127\.0\.0\.1$,^\[::1\]$'
+	      ;;
+
+        -mjb) SRC_DIR=~/src/git/GIT_mjbright/codeeurope-microservices/live-k8s-visualizer ;;
+        \.)   SRC_DIR=.;;
+        -l)   SRC_DIR=~/src/git/larrycai.gcp-live-k8s-visualizer;;
+
+	*)    die "Bad option <$1>";;
     esac
+    shift
 done
 
 set -x
-kubectl proxy --www=$SRC_DIR --www-prefix=/ --api-prefix=/api/ --port $PORT
+kubectl proxy $OPTS  --www=$SRC_DIR --www-prefix=/ --api-prefix=/api/ --port $PORT
 set +x
 
